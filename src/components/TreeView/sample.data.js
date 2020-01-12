f7f89d3b055253dd12486f7b6cfbf1c068e368c6
Nhat Thien <thienvo0211@gmail.com>
2020-01-13 00:03:09 +0700

    [WEB TEST PAGE] update UI

--- src/components/TreeView/sample.data.js
+++ src/components/TreeView/sample.data.js
@@ -1,78 +1,80 @@
 
 
 import * as Constant from './../../constants/AppConstants';
+export default class ScriptObject{
+  constructor(){
 
-var scriptObj = {
-  methodName: 'checkQuestion1',
-  expectedResult: {
-    type: 'String',
-    value: 'admin',
-    editMode: false,
-  },
-  params: [
-    {
-      label: Constant.LABEL_PARAM,
-      parentId: 39,
-      name: 'Input Parameters',
-      showChildren: true,
+  };
+  scriptObj = {
+    methodName: Constant.METHOD_NAME,
+    expectedResult: {
+      type: 'String',
+      value: 'admin',
       editMode: false,
-      children: [
-        {
-          label: Constant.LABEL_PARAM,
-          parentId: 50,
-          type:'String',
-          name: 'username',
-          value:'NguyenVanA',
-          showChildren: false,
-          editMode: false,
-          children: []
-        },
-        {
-          label: Constant.LABEL_PARAM,
-          parentId: 50,
-          type:'String',
-          name: 'password',
-          value:'p4ssw0rd',
-          showChildren: false,
-          editMode: false,
-          children: []
-        }
-      ]
     },
-    {
-
-      parentId: 39,
-      name: 'Step',
-      label: Constant.LABEL_STEP,
-      showChildren: true,
-      editMode: false,
-      children: [
-        {
-
-          label: Constant.LABEL_STEP,
-          type:'findViewById',
-          name: 'txtUsername',
-          value:'NguyenVanA',
-          showChildren: false,
-          editMode: false,
-          children: []
-        },
-        {
-
-          label: Constant.LABEL_STEP,
-          type:'findViewById',
-          name: 'txtPassword',
-          value:'p4ssw0rd',
-          showChildren: false,
-          editMode: false,
-          children: []
-        }
-      ]
-    }
-  ]
+    params: [
+      {
+        label: Constant.LABEL_PARAM,
+        parentId: 39,
+        name: 'Input Parameters',
+        showChildren: true,
+        editMode: false,
+        children: [
+          {
+            label: Constant.LABEL_PARAM,
+            parentId: 50,
+            type:'String',
+            name: 'username',
+            value:'NguyenVanA',
+            showChildren: false,
+            editMode: false,
+            children: []
+          },
+          {
+            label: Constant.LABEL_PARAM,
+            parentId: 50,
+            type:'String',
+            name: 'password',
+            value:'p4ssw0rd',
+            showChildren: false,
+            editMode: false,
+            children: []
+          }
+        ]
+      },
+      {
+  
+        parentId: 39,
+        name: 'Step',
+        label: Constant.LABEL_STEP,
+        showChildren: true,
+        editMode: false,
+        children: [
+          {
+  
+            label: Constant.LABEL_STEP,
+            type:'findViewById',
+            name: 'txtUsername',
+            value:'NguyenVanA',
+            showChildren: false,
+            editMode: false,
+            children: []
+          },
+          {
+  
+            label: Constant.LABEL_STEP,
+            type:'findViewById',
+            name: 'txtPassword',
+            value:'p4ssw0rd',
+            showChildren: false,
+            editMode: false,
+            children: []
+          }
+        ]
+      }
+    ]
+  }
 }
 
-
-export default scriptObj;
 
 
